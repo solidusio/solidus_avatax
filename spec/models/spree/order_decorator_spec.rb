4845112ae1890597f4ade1d02a980f9f98db85d7
Hoyaboya <christopher.sun@gmail.com>
2014-02-11 12:43:38 -0500

    send discount amount to avalara
    try with positive number and string
    
    set discounted on line item level
    
    get rid of the absolute value
    
    Revert "get rid of the absolute value"
    
    This reverts commit 2d0b0c5933ed0ef49a3c145a938a6c2fed6ae900.
    
    test to insure positive value for discount

--- spec/models/spree/order_decorator_spec.rb
+++ spec/models/spree/order_decorator_spec.rb
@@ -21,4 +21,23 @@ describe Spree::Order do
       order.avatax_response_at.should_not be_nil
     end
   end
+
+  describe 'promotion_adjustment_total' do
+    context 'when no adjustments' do 
+      it 'should return 0' do
+        order.promotion_adjustment_total.should == 0
+      end
+    end
+
+    context 'when there are eligible adjustments' do
+      before do
+        order.adjustments << Spree::Adjustment.create!(originator_type: "Spree::PromotionAction", amount: -5.0, order: order, label: 'Promo One')
+        order.adjustments << Spree::Adjustment.create!(originator_type: "Spree::PromotionAction", amount: -10.99, order: order, label: 'Promo Two')
+      end
+
+      it 'should have a promotion_adjustment_total of 10.99' do
+        order.promotion_adjustment_total.should == 10.99
+      end
+    end
+  end
 end
