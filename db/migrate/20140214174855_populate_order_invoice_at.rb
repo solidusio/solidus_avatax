c3d43e4f1d45896463c2273caab506f915a48055
Hoyaboya <christopher.sun@gmail.com>
2014-02-24 13:59:43 -0500

    make magnus changes

--- db/migrate/20140214174855_populate_order_invoice_at.rb
+++ db/migrate/20140214174855_populate_order_invoice_at.rb
@@ -1,19 +1,10 @@
 class PopulateOrderInvoiceAt < ActiveRecord::Migration
   def up 
     # For legacy orders, assume that if we calculated an Avatax for the order, we also invoiced it.
-    Spree::Order.all.each do |order|
-      if order.avatax_response_at
-        order.update_attribute(:avatax_invoice_at, order.avatax_response_at)
-      end
-    end
+    Spree::Order.update_all "avatax_invoice_at = avatax_response_at"
   end
 
   def down
-    # For legacy orders, assume that if we calculated an Avatax for the order, we also invoiced it.
-    Spree::Order.all.each do |order|
-      if order.avatax_response_at
-        order.update_attribute(:avatax_invoice_at, nil)
-      end
-    end
+    raise ActiveRecord::IrreversibleMigration
   end
 end
