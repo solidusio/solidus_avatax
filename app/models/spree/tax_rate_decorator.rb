69ca6826301f6a6dccc818ad436e11eb7f2e438e
Jordan Brough <git.j@brgh.net>
2014-06-17 13:17:15 -0600

    minor reformat
    move "def self.X" into "class << self..." block. preparation for adding more class methods.

--- app/models/spree/tax_rate_decorator.rb
+++ app/models/spree/tax_rate_decorator.rb
@@ -1,15 +1,17 @@
 Spree::TaxRate.class_eval do
   validate :avatax_there_can_be_only_one, on: :create
 
-  # require exactly one tax rate.  if that's not true then alert ourselves and carry on as best we can
-  def self.avatax_the_one_rate
-    rates = all.to_a
-    if rates.size != 1
-      if defined?(Honeybadger)
-        Honeybadger.notify("#{rates.size} tax rates detected and there should be only one")
+  class << self
+    # require exactly one tax rate.  if that's not true then alert ourselves and carry on as best we can
+    def avatax_the_one_rate
+      rates = all.to_a
+      if rates.size != 1
+        if defined?(Honeybadger)
+          Honeybadger.notify("#{rates.size} tax rates detected and there should be only one")
+        end
       end
+      rates.sort_by(&:id).first
     end
-    rates.sort_by(&:id).first
   end
 
   def avatax_there_can_be_only_one
