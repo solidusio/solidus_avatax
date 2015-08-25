c469ac48e7db6d915c61f1d67605a17d7fdf8248
Jordan Brough <git.j@brgh.net>
2015-08-25 08:27:43 -0600

    Remove Address after_save hook
    Address is immutable in Solidus >= 1.1.0 so this code is useless and
    the specs break when they try to mutate addresses.

--- app/models/spree/address_decorator.rb
+++
@@ -1,9 +0,0 @@
-Spree::Address.class_eval do
-  after_save :avatax_clear_tax
-
-  def avatax_clear_tax
-    Spree::Order.incomplete.where(ship_address_id: id).each do |order|
-      SpreeAvatax::SalesShared.reset_tax_attributes(order)
-    end
-  end
-end
