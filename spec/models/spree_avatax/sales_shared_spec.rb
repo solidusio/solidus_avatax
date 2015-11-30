26f3d01319ab48f9a018cd2deee8be60bc16cb64
Jordan Brough <git.j@brgh.net>
2015-11-30 10:46:13 -0800

    Remove SalesInvoice while clearing tax attributes
    Otherwise it's possible for this to happen:
    
    - Get the order into the "confirm" state with a valid invoice
    - Move the order backward into a prior state
    - Move to the confirm state but fail to generate a new invoice (e.g.
    during an Avatax outage)
    - Complete the order and inadvertently commit an invalid invoice
    
    Also: Only create a SalesInvoice after we successfully apply the tax
    adjustments.

--- spec/models/spree_avatax/sales_shared_spec.rb
+++ spec/models/spree_avatax/sales_shared_spec.rb
@@ -58,5 +58,28 @@ describe SpreeAvatax::SalesShared do
       expect(line_item.reload.pre_tax_amount).to eq(2 * 3)
       expect(shipment.reload.pre_tax_amount).to eq(5)
     end
+
+    context 'when a SalesInvoice record is present' do
+      let!(:sales_invoice) { create(:avatax_sales_invoice, order: order) }
+
+      it 'deletes the SalesInvoice record if present' do
+        subject
+        expect(order.reload.avatax_sales_invoice).to eq(nil)
+      end
+
+      context 'when the SalesInvoice is committed' do
+        before do
+          sales_invoice.update!(committed_at: Time.now)
+        end
+
+        it 'raises without clearing anything' do
+          expect {
+            subject
+          }.to raise_error(SpreeAvatax::SalesInvoice::AlreadyCommittedError)
+
+          expect(line_item.adjustments.tax.count).to eq(1)
+        end
+      end
+    end
   end
 end
