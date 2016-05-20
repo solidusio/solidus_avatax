5d36065e64a2d732ae3e0119b59e38ca07f17368
Bryan Mahoney <bryan@glossier.com>
2016-05-19 22:03:14 -0400

    Generate the invoice earlier on in the checkout process

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -6,9 +6,7 @@ Spree::Order.class_eval do
 
   state_machine.after_transition from: :address do |order, transition|
     SpreeAvatax::SalesShared.reset_tax_attributes(order)
-  end
-
-  state_machine.before_transition to: :payment do |order, transition|
+    # This needs to be called earlier because of how the checkout works on Glossier
     SpreeAvatax::SalesInvoice.generate(order)
   end
 
