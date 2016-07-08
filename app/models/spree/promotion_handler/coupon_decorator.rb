0a2c1ecee8d8b64a8406ff907815f93ec2b2c4cf
Joe Lind <joelind@gmail.com>
2016-07-08 08:41:01 -0400

    Use `prepend` instead of `alias_method_chain`
    * `alias_method_chain` causes errors when using background jobs to process orders with sidekiq (see https://github.com/mperham/sidekiq/issues/2139)

--- app/models/spree/promotion_handler/coupon_decorator.rb
+++ app/models/spree/promotion_handler/coupon_decorator.rb
@@ -1,11 +1,11 @@
 Spree::PromotionHandler::Coupon.class_eval do
-  def apply_with_avatax
-    apply_without_avatax.tap do
-      if successful?
-        SpreeAvatax::SalesShared.reset_tax_attributes(order)
+  prepend Module.new do
+    def apply
+      super.tap do
+        if successful?
+          SpreeAvatax::SalesShared.reset_tax_attributes(order)
+        end
       end
     end
   end
-
-  alias_method_chain :apply, :avatax
 end
