60cf899085a235f0db88670efde123cf22343a09
Joe Lind <joelind@gmail.com>
2016-07-08 08:41:01 -0400

    Use `prepend` instead of `alias_method_chain`
    * `alias_method_chain` causes errors when using background jobs to process orders with sidekiq (see https://github.com/mperham/sidekiq/issues/2139)

--- app/models/spree/tax/order_adjuster_decorator.rb
+++ app/models/spree/tax/order_adjuster_decorator.rb
@@ -1,9 +1,9 @@
 Spree::Tax::OrderAdjuster.class_eval do
-  def adjust_with_avatax!
-    # do nothing. we hook in in our own ways.
-    # TODO: See if we can make OrderAdjuster pluggable and workable for what we
-    # need to do.
+  prepend Module.new do
+    def adjust!
+      # do nothing. we hook in in our own ways.
+      # TODO: See if we can make OrderAdjuster pluggable and workable for what we
+      # need to do.
+    end
   end
-
-  alias_method_chain :adjust!, :avatax
 end
