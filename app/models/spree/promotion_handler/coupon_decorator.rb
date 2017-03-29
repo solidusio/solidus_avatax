4ec41728afe006bf99df25c48bc078d2c0e8e6cb
Yana Agun Siswanto <yana.developer@gmail.com>
2017-03-29 16:43:56 +0700

    Fix Deprecation Warning. #27

--- app/models/spree/promotion_handler/coupon_decorator.rb
+++ app/models/spree/promotion_handler/coupon_decorator.rb
@@ -1,11 +1,15 @@
-Spree::PromotionHandler::Coupon.class_eval do
-  def apply_with_avatax
-    apply_without_avatax.tap do
-      if successful?
-        SpreeAvatax::SalesShared.reset_tax_attributes(order)
+module Solidus
+  module Avatax
+    module PromotionHandler
+      def apply
+        super.tap do
+          if successful?
+            SpreeAvatax::SalesShared.reset_tax_attributes(order)
+          end
+        end
       end
     end
   end
-
-  alias_method_chain :apply, :avatax
 end
+
+Spree::PromotionHandler::Coupon.send(:prepend, Solidus::Avatax::PromotionHandler)
--- app/models/spree/tax/order_adjuster_decorator.rb
+++ app/models/spree/tax/order_adjuster_decorator.rb
@@ -1,9 +1,9 @@
-Spree::Tax::OrderAdjuster.class_eval do
-  def adjust_with_avatax!
+module Solidus::Avatax::OrderAdjuster
+  def adjust!
     # do nothing. we hook in in our own ways.
     # TODO: See if we can make OrderAdjuster pluggable and workable for what we
     # need to do.
   end
-
-  alias_method_chain :adjust!, :avatax
 end
+
+Spree::Tax::OrderAdjuster.send(:prepend, Solidus::Avatax::OrderAdjuster)
