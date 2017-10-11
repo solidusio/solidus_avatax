646544e0cb474f196c4367ce3305aed6fdc408f0
Braden <braden.douglass@gmail.com>
2017-10-11 16:46:25 -0400

    Don't Commit Unless We Have An Avatax Invoice.

--- app/jobs/commit_sales_invoice_job.rb
+++ app/jobs/commit_sales_invoice_job.rb
@@ -4,6 +4,7 @@ class CommitSalesInvoiceJob < ActiveJob::Base
   def perform(order_id)
     order = ::Spree::Order.find(order_id)
     return if order.pos_order?
+    return unless order.avatax_sales_invoice
     SpreeAvatax::SalesInvoice.commit(order)
   end
 end
